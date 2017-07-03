435bfb87c3f1669b5808e4092221e6d8ab32431c
Bruce Li <leilmyxwz@gmail.com>
2017-07-02 23:59:34 -0700

    add a guard for extreme dpi computation case.

--- src/wayland/display_metrics.cc
+++ src/wayland/display_metrics.cc
@@ -18,6 +18,10 @@ DisplayMetrics::DisplayMetrics(int32_t w, int32_t h, int32_t pw, int32_t ph) {
 
   scale = round(sqrt(w * w + h * h) / sqrt(pw * pw + ph * ph) / k1xPixelPerMm);
 
+  // Minimum of 1x scale is expected.
+  if (scale < 1)
+    scale = 1;
+
   width_dp = width_pixels / scale;
   height_dp = height_pixels / scale;
 }
